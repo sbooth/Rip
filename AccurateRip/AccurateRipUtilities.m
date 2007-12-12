6d3a1ca89d5a4b87dc7418beda4f292aa18d6068
Stephen F. Booth <me@sbooth.org>
2007-12-11 22:04:16 -0800

    calculateAccurateRipCRCForFile can read multiple formats
    calculateAccurateRipCRCForFile now uses the ExtAudioFile API for reading, allowing multiple file formats to
    be supported.

--- AccurateRip/AccurateRipUtilities.m
+++ AccurateRip/AccurateRipUtilities.m
@@ -5,14 +5,39 @@
 
 #include "AccurateRipUtilities.h"
 
+#include <AudioToolbox/ExtendedAudioFile.h>
 #include <IOKit/storage/IOCDTypes.h>
 
-#include <sys/stat.h>
-#include <stdio.h>
-
 // A block of CDDA audio consists of 2352 bytes and contains 588 frames of 16-bit 2  (!! or 4) channel audio
+#define FRAMES_PER_SECTOR	588u
 #define BYTES_PER_SAMPLE	(16 / 8)
 #define FRAMES_PER_BLOCK	(kCDSectorSizeCDDA / (2 * BYTES_PER_SAMPLE))
+
+// ========================================
+// Verify an AudioStreamBasicDescription describes CDDA audio
+// ========================================
+static BOOL
+streamDescriptionIsCDDA(const AudioStreamBasicDescription *asbd)
+{
+	NSCParameterAssert(NULL != asbd);
+	
+	if(kAudioFormatLinearPCM != asbd->mFormatID)
+		return NO;
+
+	if(!(kAudioFormatFlagIsSignedInteger & asbd->mFormatFlags) || !((kAudioFormatFlagIsPacked & asbd->mFormatFlags)))
+		return NO;
+	
+	if(44100 != asbd->mSampleRate)
+		return NO;
+
+	if(2 != asbd->mChannelsPerFrame)
+		return NO;
+	
+	if(16 != asbd->mBitsPerChannel)
+		return NO;
+	
+	return YES;
+}
 
 // ========================================
 // Calculate and AccurateRip CRC for the file at path (must be raw 16-bit little-endian signed PCM)
@@ -22,32 +47,61 @@ calculateAccurateRipCRCForFile(NSString *path, BOOL firstTrack, BOOL lastTrack)
 {
 	NSCParameterAssert(nil != path);
 	
-	if(![[NSFileManager defaultManager] fileExistsAtPath:path])
-		return 0;
-	
-	// Determine the size of the file and the number of sectors it contains
-	struct stat buf;
-	if(-1 == stat(path.fileSystemRepresentation, &buf))
-		return 0;
-	
-	NSUInteger totalBlocks = (NSUInteger)(buf.st_size / kCDSectorSizeCDDA);
-	
-	// Open the file for reading
-	NSFileHandle *fileHandle = [NSFileHandle fileHandleForReadingAtPath:path];
-	if(nil == fileHandle)
-		return 0;
-	
 	uint32_t crc = 0;
+
+	// Open the file for reading
+	ExtAudioFileRef file = NULL;
+	OSStatus status = ExtAudioFileOpenURL((CFURLRef)[NSURL fileURLWithPath:path], &file);
+	if(noErr != status)
+		return 0;
+	
+	// Verify the file contains CDDA audio
+	AudioStreamBasicDescription fileFormat;
+	UInt32 dataSize = sizeof(fileFormat);
+	status = ExtAudioFileGetProperty(file, kExtAudioFileProperty_FileDataFormat, &dataSize, &fileFormat);
+	if(noErr != status)
+		goto cleanup;
+	
+	if(!streamDescriptionIsCDDA(&fileFormat))
+		goto cleanup;
+	
+	// Determine the total number of audio frames in the file
+	SInt64 totalFrames = -1;
+	dataSize = sizeof(totalFrames);
+	status = ExtAudioFileGetProperty(file, kExtAudioFileProperty_FileLengthFrames, &dataSize, &totalFrames);
+	if(noErr != status)
+		goto cleanup;
+	
+	// Convert the number of frames to the number of blocks (CDDA sectors)
+	NSUInteger totalBlocks = (NSUInteger)(totalFrames / FRAMES_PER_BLOCK);
 	NSUInteger blockNumber = 0;
 
+	// Set up extraction buffers
+	int8_t buffer [kCDSectorSizeCDDA];
+	AudioBufferList bufferList;
+	bufferList.mNumberBuffers = 1;
+	bufferList.mBuffers[0].mNumberChannels = fileFormat.mChannelsPerFrame;
+	bufferList.mBuffers[0].mData = (void *)buffer;	
+	
+	// Iteratively process each CDDA sector in the file
 	for(;;) {
-		NSData *block = [fileHandle readDataOfLength:kCDSectorSizeCDDA];
+		bufferList.mBuffers[0].mDataByteSize = kCDSectorSizeCDDA;
 		
-		if(kCDSectorSizeCDDA != block.length)
+		UInt32 frameCount = FRAMES_PER_SECTOR;
+		status = ExtAudioFileRead(file, &frameCount, &bufferList);
+		
+		if(noErr != status)
+			break;
+		else if(FRAMES_PER_SECTOR != frameCount)
 			break;
 		
-		crc += calculateAccurateRipCRCForBlock(block.bytes, blockNumber++, totalBlocks, firstTrack, lastTrack);
+		crc += calculateAccurateRipCRCForBlock(buffer, blockNumber++, totalBlocks, firstTrack, lastTrack);
 	}
+	
+cleanup:
+	status = ExtAudioFileDispose(file);
+/*	if(noErr != status)
+		return 0;*/
 	
 	return crc;
 }
