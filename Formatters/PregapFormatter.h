0ff5e2d9fcbd96060af828cdf8ac2c1565c326bb
Stephen F. Booth <me@sbooth.org>
2008-08-21 20:02:03 -0700

    Created pregap formatter to correctly format CDMSF values without the 150 frame adjustment

---
+++ Formatters/PregapFormatter.h
@@ -0,0 +1,15 @@
+/*
+ *  Copyright (C) 2007 - 2008 Stephen F. Booth <me@sbooth.org>
+ *  All Rights Reserved
+ */
+
+#import <Cocoa/Cocoa.h>
+
+// ========================================
+// Interprets an NSInteger as a CDMSF and formats it as MM:SS.FF
+// ========================================
+@interface PregapFormatter : NSFormatter
+{
+}
+
+@end
---
+++ Formatters/PregapFormatter.m
@@ -0,0 +1,63 @@
+/*
+ *  Copyright (C) 2007 - 2008 Stephen F. Booth <me@sbooth.org>
+ *  All Rights Reserved
+ */
+
+#import "PregapFormatter.h"
+
+#include <IOKit/storage/IOCDTypes.h>
+
+@implementation PregapFormatter
+
+- (NSString *) stringForObjectValue:(id)anObject
+{
+	if(nil == anObject || ! [anObject respondsToSelector:@selector(integerValue)])
+		return nil;
+	
+	NSUInteger offset = [anObject integerValue];
+	CDMSF msf = CDConvertLBAToMSF(offset - 150);
+	
+	return [NSString stringWithFormat:@"%02i:%02i.%02i", msf.minute, msf.second, msf.frame];
+}
+
+- (BOOL) getObjectValue:(id *)object forString:(NSString *)string errorDescription:(NSString  **)error
+{
+	CDMSF		msf			= { 0xFF, 0xFF, 0xFF };
+	BOOL		result		= NO;
+	NSInteger	value;
+	
+	NSScanner *scanner = [NSScanner scannerWithString:string];
+	
+	if([scanner scanInteger:&value]) {
+		msf.minute = value;
+		result = YES;
+	}
+	
+	[scanner scanString:@":" intoString:NULL];
+	
+	if([scanner scanInteger:&value]) {
+		msf.second = value;
+		result = YES;
+	}
+	
+	[scanner scanString:@"." intoString:NULL];
+	
+	if([scanner scanInteger:&value]) {
+		msf.frame = value;
+		result = YES;
+	}
+	
+	if(result && NULL != object)
+		*object = [NSNumber numberWithUnsignedInt:(CDConvertMSFToLBA(msf) + 150)];
+	else if(NULL != error)
+		*error = @"Couldn't convert MM:SS.FF to CDMSF";
+	
+	return result;
+}
+
+- (NSAttributedString *) attributedStringForObjectValue:(id)object withDefaultAttributes:(NSDictionary *)attributes
+{
+	return [[NSAttributedString alloc] initWithString:[self stringForObjectValue:object] attributes:attributes];
+}
+
+@end
