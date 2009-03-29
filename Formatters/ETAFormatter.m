d5fa6e3d7b5ff30cd2f87a318afbbeef510b38f8
Stephen F. Booth <me@sbooth.org>
2009-03-28 22:30:35 -0700

    Added localization wrappers

--- Formatters/ETAFormatter.m
+++ Formatters/ETAFormatter.m
@@ -33,38 +33,38 @@
 	
 	if(intervalInMinutes >= 0 && intervalInMinutes <= 1) {
 		if(!self.includeSeconds)
-			return (intervalInMinutes == 0 ? @"less than a minute" : @"1 minute");
+			return (intervalInMinutes == 0 ? NSLocalizedString(@"less than a minute", @"") : NSLocalizedString(@"1 minute", @""));
 		if(intervalInSeconds >= 0 && intervalInSeconds <= 4)
-			return @"less than 5 seconds";
+			return NSLocalizedString(@"less than 5 seconds", @"");
 		else if(intervalInSeconds >= 5 && intervalInSeconds <= 9) 
-			return @"less than 10 seconds";
+			return NSLocalizedString(@"less than 10 seconds", @"");
 		else if(intervalInSeconds >= 10 && intervalInSeconds <= 19) 
-			return @"less than 20 seconds";
+			return NSLocalizedString(@"less than 20 seconds", @"");
 		else if(intervalInSeconds >= 20 && intervalInSeconds <= 39) 
-			return @"half a minute";
+			return NSLocalizedString(@"half a minute", @"");
 		else if(intervalInSeconds >= 40 && intervalInSeconds <= 59) 
-			return @"less than a minute";
+			return NSLocalizedString(@"less than a minute", @"");
 		else 
-			return @"1 minute";
+			return NSLocalizedString(@"1 minute", @"");
 	}
 	else if(intervalInMinutes >= 2 && intervalInMinutes <= 44) 
-		return [NSString stringWithFormat:@"%.0f minutes", intervalInMinutes];
+		return [NSString stringWithFormat:NSLocalizedString(@"%.0f minutes", @""), intervalInMinutes];
 	else if(intervalInMinutes >= 45 && intervalInMinutes <= 89) 
-		return @"about 1 hour";
+		return NSLocalizedString(@"about 1 hour", @"");
 	else if(intervalInMinutes >= 90 && intervalInMinutes <= 1439) 
-		return [NSString stringWithFormat:@"about %.0f hours", round(intervalInMinutes / MINUTES_PER_HOUR)];
+		return [NSString stringWithFormat:NSLocalizedString(@"about %.0f hours", @""), round(intervalInMinutes / MINUTES_PER_HOUR)];
 	else if(intervalInMinutes >= 1440 && intervalInMinutes <= 2879) 
-		return @"1 day";
+		return NSLocalizedString(@"1 day", @"");
 	else if(intervalInMinutes >= 2880 && intervalInMinutes <= 43199) 
-		return [NSString stringWithFormat:@"%.0f days", round(intervalInMinutes / 1440.0)];
+		return [NSString stringWithFormat:NSLocalizedString(@"%.0f days", @""), round(intervalInMinutes / 1440.0)];
 	else if(intervalInMinutes >= 43200 && intervalInMinutes <= 86399) 
-		return @"about 1 month";
+		return NSLocalizedString(@"about 1 month", @"");
 	else if(intervalInMinutes >= 86400 && intervalInMinutes <= 525599) 
-		return [NSString stringWithFormat:@"%.0f months", round(intervalInMinutes / 43200.0)];
+		return [NSString stringWithFormat:NSLocalizedString(@"%.0f months", @""), round(intervalInMinutes / 43200.0)];
 	else if(intervalInMinutes >= 525600 && intervalInMinutes <= 1051199) 
-		return @"about 1 year";
+		return NSLocalizedString(@"about 1 year", @"");
 	else
-		return [NSString stringWithFormat:@"over %.0f years", round(intervalInMinutes / 525600.0)];
+		return [NSString stringWithFormat:NSLocalizedString(@"over %.0f years", @""), round(intervalInMinutes / 525600.0)];
 }
 
 - (NSAttributedString *) attributedStringForObjectValue:(id)object withDefaultAttributes:(NSDictionary *)attributes
