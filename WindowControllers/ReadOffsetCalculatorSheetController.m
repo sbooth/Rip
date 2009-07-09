10d7eeccb95e21ec688b9b823bc6e95016e395df
Stephen F. Booth <me@sbooth.org>
2009-07-08 23:24:21 -0700

    Improved logic to only check tracks that are present in AR with an offset checksum

--- WindowControllers/ReadOffsetCalculatorSheetController.m
+++ WindowControllers/ReadOffsetCalculatorSheetController.m
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2008 Stephen F. Booth <me@sbooth.org>
+ *  Copyright (C) 2008 - 2009 Stephen F. Booth <me@sbooth.org>
  *  All Rights Reserved
  */
 
@@ -49,6 +49,7 @@ static NSString * const kCalculateOffsetsKVOContext		= @"org.sbooth.Rip.ReadOffs
 @end
 
 @interface ReadOffsetCalculatorSheetController (Private)
+- (void) accurateRipQueryOperationDidFinish:(AccurateRipQueryOperation *)operation;
 - (void) readOffsetCalculationOperationDidFinish:(ReadOffsetCalculationOperation *)operation;
 @end
 
@@ -69,6 +70,9 @@ static NSString * const kCalculateOffsetsKVOContext		= @"org.sbooth.Rip.ReadOffs
 		// Create our own context for accessing the store
 		self.managedObjectContext = [[NSManagedObjectContext alloc] init];
 		[self.managedObjectContext setPersistentStoreCoordinator:[[[NSApplication sharedApplication] delegate] persistentStoreCoordinator]];
+
+		// Register to receive NSManagedObjectContextDidSaveNotification to keep our MOC in sync
+		[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(managedObjectContextDidSave:) name:NSManagedObjectContextDidSaveNotification object:nil];
 
 		self.operationQueue = [[NSOperationQueue alloc] init];
 
@@ -121,6 +125,14 @@ static NSString * const kCalculateOffsetsKVOContext		= @"org.sbooth.Rip.ReadOffs
 				didPresentSelector:@selector(didPresentErrorWithRecovery:contextInfo:) 
 					   contextInfo:NULL];
 			}
+			
+			if([operation isFinished]) {
+				if([NSThread isMainThread])
+					[self accurateRipQueryOperationDidFinish:operation];
+				else
+					[self performSelectorOnMainThread:@selector(accurateRipQueryOperationDidFinish:) withObject:operation waitUntilDone:NO];
+			}
+			
 		}
 	}
 	else if(kExtractAudioKVOContext == context) {
@@ -166,6 +178,16 @@ static NSString * const kCalculateOffsetsKVOContext		= @"org.sbooth.Rip.ReadOffs
 	}
 	else
 		[super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
+}
+
+- (void) managedObjectContextDidSave:(NSNotification *)notification
+{
+	NSParameterAssert(nil != notification);
+	
+	// "Auto-refresh" objects changed in another MOC
+	NSManagedObjectContext *managedObjectContext = [notification object];
+	if(managedObjectContext != self.managedObjectContext)
+		[self.managedObjectContext mergeChangesFromContextDidSaveNotification:notification];
 }
 
 #pragma mark NSWindow Delegate Methods
@@ -224,68 +246,12 @@ static NSString * const kCalculateOffsetsKVOContext		= @"org.sbooth.Rip.ReadOffs
 
 	accurateRipQueryOperation.compactDiscID = self.compactDisc.objectID;
 	
-	// Extract a portion of the first track on the disc that is at least six seconds long
-	TrackDescriptor *trackToExtract = nil;
-	for(TrackDescriptor *potentialTrack in self.compactDisc.firstSession.tracks) {
-		// The track must be at least six seconds long (plus the buffer); if it isn't, skip it
-		if(((6 * CDDA_SECTORS_PER_SECOND) + MAXIMUM_OFFSET_TO_CHECK_IN_SECTORS) > potentialTrack.sectorCount)
-			continue;
-		
-		trackToExtract = potentialTrack;
-		break;
-	}
-
-	if(!trackToExtract) {
-		NSBeep();
-		// TODO: Descriptive error message
-		return;
-	}
-
-	SectorRange *trackSectorRange = [trackToExtract sectorRange];
-	
-	// AccurateRip offset checksums start at six seconds into the file
-	NSUInteger sixSecondPointSector = trackSectorRange.firstSector + (6 * CDDA_SECTORS_PER_SECOND);
-	
-	SectorRange *sectorsToExtract = [SectorRange sectorRangeWithFirstSector:(sixSecondPointSector - MAXIMUM_OFFSET_TO_CHECK_IN_SECTORS)
-																sectorCount:((2 * MAXIMUM_OFFSET_TO_CHECK_IN_SECTORS) + 1)];
-
-	ExtractionOperation *extractionOperation = [[ExtractionOperation alloc] init];
-	
-	extractionOperation.disk = self.disk;
-	extractionOperation.sectors = sectorsToExtract;
-	extractionOperation.allowedSectors = self.compactDisc.firstSession.sectorRange;
-	extractionOperation.URL = temporaryURLWithExtension(@"wav");
-	extractionOperation.useC2 = NO;
-
-	// Offset calculation
-	ReadOffsetCalculationOperation *offsetCalculationOperation = [[ReadOffsetCalculationOperation alloc] init];
-	
-	offsetCalculationOperation.URL = extractionOperation.URL;
-	offsetCalculationOperation.trackID = trackToExtract.objectID;
-	offsetCalculationOperation.sixSecondPointSector = MAXIMUM_OFFSET_TO_CHECK_IN_SECTORS;
-	offsetCalculationOperation.maximumOffsetToCheck = (MAXIMUM_OFFSET_TO_CHECK_IN_SECTORS * AUDIO_FRAMES_PER_CDDA_SECTOR);
-
-	// Set up operation dependencies
-	[extractionOperation addDependency:accurateRipQueryOperation];
-	[offsetCalculationOperation addDependency:extractionOperation];
-	
 	// Observe the operations' state
 	[accurateRipQueryOperation addObserver:self forKeyPath:@"isExecuting" options:NSKeyValueObservingOptionNew context:kQueryAccurateRipKVOContext];
 	[accurateRipQueryOperation addObserver:self forKeyPath:@"isCancelled" options:NSKeyValueObservingOptionNew context:kQueryAccurateRipKVOContext];
 	[accurateRipQueryOperation addObserver:self forKeyPath:@"isFinished" options:NSKeyValueObservingOptionNew context:kQueryAccurateRipKVOContext];
-	
-	[extractionOperation addObserver:self forKeyPath:@"isExecuting" options:NSKeyValueObservingOptionNew context:kExtractAudioKVOContext];
-	[extractionOperation addObserver:self forKeyPath:@"isCancelled" options:NSKeyValueObservingOptionNew context:kExtractAudioKVOContext];
-	[extractionOperation addObserver:self forKeyPath:@"isFinished" options:NSKeyValueObservingOptionNew context:kExtractAudioKVOContext];
-	
-	[offsetCalculationOperation addObserver:self forKeyPath:@"isExecuting" options:NSKeyValueObservingOptionNew context:kCalculateOffsetsKVOContext];
-	[offsetCalculationOperation addObserver:self forKeyPath:@"isCancelled" options:NSKeyValueObservingOptionNew context:kCalculateOffsetsKVOContext];
-	[offsetCalculationOperation addObserver:self forKeyPath:@"isFinished" options:NSKeyValueObservingOptionNew context:kCalculateOffsetsKVOContext];
-	
-	// Go!
+		
 	[self.operationQueue addOperation:accurateRipQueryOperation];
-	[self.operationQueue addOperation:extractionOperation];
-	[self.operationQueue addOperation:offsetCalculationOperation];
 }
 
 - (IBAction) acceptSuggestedOffset:(id)sender
@@ -396,6 +362,116 @@ static NSString * const kCalculateOffsetsKVOContext		= @"org.sbooth.Rip.ReadOffs
 @end
 
 @implementation ReadOffsetCalculatorSheetController (Private)
+
+- (void) accurateRipQueryOperationDidFinish:(AccurateRipQueryOperation *)operation
+{
+	NSParameterAssert(nil != operation);
+	
+	// If the operation didn't succeed, it isn't worthwhile to continue
+	if(!self.compactDisc.accurateRipDiscs) {
+		NSMutableDictionary *errorDictionary = [NSMutableDictionary dictionary];
+		
+		[errorDictionary setObject:NSLocalizedString(@"This disc cannot be used to calculate the read offset.", @"") forKey:NSLocalizedDescriptionKey];
+		[errorDictionary setObject:NSLocalizedString(@"Drive offsets can only be determined using discs present in the AccurateRip database.", @"") forKey:NSLocalizedRecoverySuggestionErrorKey];
+		
+		NSError *error = [NSError errorWithDomain:NSURLErrorDomain code:NSURLErrorNotConnectedToInternet userInfo:errorDictionary];
+		
+		[self presentError:error modalForWindow:self.window delegate:self didPresentSelector:@selector(didPresentErrorWithRecovery:contextInfo:) contextInfo:NULL];
+
+		return;
+	}
+	
+	// Form a union of the tracks that can be checked. A track can be checked if:
+	// - It has an offset checksum from AccurateRip
+	// - It is at least six seconds long (if it isn't it shouldn't have an offset checksum, but who knows)
+
+	// First grab the tracks that contain offset checksums
+	NSSet *allAccurateRipTracks = [self.compactDisc.accurateRipDiscs valueForKeyPath:@"@distinctUnionOfSets.tracks"];
+	NSPredicate *accurateRipTracksPredicate = [NSPredicate predicateWithFormat:@"offsetChecksum != NULL"];
+	NSSet *accurateRipTracksWithOffsetChecksums = [allAccurateRipTracks filteredSetUsingPredicate:accurateRipTracksPredicate];
+
+	// If none were found, nothing can be done
+	if(![accurateRipTracksWithOffsetChecksums count]) {
+		NSMutableDictionary *errorDictionary = [NSMutableDictionary dictionary];
+		
+		[errorDictionary setObject:NSLocalizedString(@"This disc cannot be used to calculate the read offset.", @"") forKey:NSLocalizedDescriptionKey];
+		[errorDictionary setObject:NSLocalizedString(@"Drive offsets can only be determined with AccurateRip key discs. This disc is not a key disc.", @"") forKey:NSLocalizedRecoverySuggestionErrorKey];
+		
+		NSError *error = [NSError errorWithDomain:NSURLErrorDomain code:NSURLErrorNotConnectedToInternet userInfo:errorDictionary];
+		
+		[self presentError:error modalForWindow:self.window delegate:self didPresentSelector:@selector(didPresentErrorWithRecovery:contextInfo:) contextInfo:NULL];
+		
+		return;
+	}
+
+	// And grab the track objects that correspond to them
+	NSArray *trackNumbers = [accurateRipTracksWithOffsetChecksums valueForKey:@"number"];
+	NSPredicate *potentialTracksPredicate = [NSPredicate predicateWithFormat:@"number IN %@", trackNumbers];
+	NSSet *potentialTracks = [self.compactDisc.firstSession.tracks filteredSetUsingPredicate:potentialTracksPredicate];
+
+	// Extract a portion of the first track meeting our criteria that is at least six seconds long
+	TrackDescriptor *trackToExtract = nil;
+	for(TrackDescriptor *potentialTrack in potentialTracks) {
+		// The track must be at least six seconds long (plus the buffer); if it isn't, skip it
+		if(((6 * CDDA_SECTORS_PER_SECOND) + MAXIMUM_OFFSET_TO_CHECK_IN_SECTORS) > potentialTrack.sectorCount)
+			continue;
+		
+		trackToExtract = potentialTrack;
+		break;
+	}
+	
+	if(!trackToExtract) {
+		NSMutableDictionary *errorDictionary = [NSMutableDictionary dictionary];
+		
+		[errorDictionary setObject:NSLocalizedString(@"This disc cannot be used to calculate the read offset.", @"") forKey:NSLocalizedDescriptionKey];
+		[errorDictionary setObject:NSLocalizedString(@"This disc does not contain any tracks that can be used for offset determination.", @"") forKey:NSLocalizedRecoverySuggestionErrorKey];
+		
+		NSError *error = [NSError errorWithDomain:NSURLErrorDomain code:NSURLErrorNotConnectedToInternet userInfo:errorDictionary];
+		
+		[self presentError:error modalForWindow:self.window delegate:self didPresentSelector:@selector(didPresentErrorWithRecovery:contextInfo:) contextInfo:NULL];
+
+		return;
+	}
+	
+	SectorRange *trackSectorRange = [trackToExtract sectorRange];
+	
+	// AccurateRip offset checksums start at six seconds into the file
+	NSUInteger sixSecondPointSector = trackSectorRange.firstSector + (6 * CDDA_SECTORS_PER_SECOND);
+	
+	SectorRange *sectorsToExtract = [SectorRange sectorRangeWithFirstSector:(sixSecondPointSector - MAXIMUM_OFFSET_TO_CHECK_IN_SECTORS)
+																sectorCount:((2 * MAXIMUM_OFFSET_TO_CHECK_IN_SECTORS) + 1)];
+	
+	ExtractionOperation *extractionOperation = [[ExtractionOperation alloc] init];
+	
+	extractionOperation.disk = self.disk;
+	extractionOperation.sectors = sectorsToExtract;
+	extractionOperation.allowedSectors = self.compactDisc.firstSession.sectorRange;
+	extractionOperation.URL = temporaryURLWithExtension(@"wav");
+	extractionOperation.useC2 = NO;
+	
+	// Offset calculation
+	ReadOffsetCalculationOperation *offsetCalculationOperation = [[ReadOffsetCalculationOperation alloc] init];
+	
+	offsetCalculationOperation.URL = extractionOperation.URL;
+	offsetCalculationOperation.trackID = trackToExtract.objectID;
+	offsetCalculationOperation.sixSecondPointSector = MAXIMUM_OFFSET_TO_CHECK_IN_SECTORS;
+	offsetCalculationOperation.maximumOffsetToCheck = (MAXIMUM_OFFSET_TO_CHECK_IN_SECTORS * AUDIO_FRAMES_PER_CDDA_SECTOR);
+	
+	// Set up operation dependencies
+	[offsetCalculationOperation addDependency:extractionOperation];
+	
+	[extractionOperation addObserver:self forKeyPath:@"isExecuting" options:NSKeyValueObservingOptionNew context:kExtractAudioKVOContext];
+	[extractionOperation addObserver:self forKeyPath:@"isCancelled" options:NSKeyValueObservingOptionNew context:kExtractAudioKVOContext];
+	[extractionOperation addObserver:self forKeyPath:@"isFinished" options:NSKeyValueObservingOptionNew context:kExtractAudioKVOContext];
+	
+	[offsetCalculationOperation addObserver:self forKeyPath:@"isExecuting" options:NSKeyValueObservingOptionNew context:kCalculateOffsetsKVOContext];
+	[offsetCalculationOperation addObserver:self forKeyPath:@"isCancelled" options:NSKeyValueObservingOptionNew context:kCalculateOffsetsKVOContext];
+	[offsetCalculationOperation addObserver:self forKeyPath:@"isFinished" options:NSKeyValueObservingOptionNew context:kCalculateOffsetsKVOContext];
+	
+	// Go!
+	[self.operationQueue addOperation:extractionOperation];
+	[self.operationQueue addOperation:offsetCalculationOperation];	
+}
 
 - (void) readOffsetCalculationOperationDidFinish:(ReadOffsetCalculationOperation *)operation
 {
